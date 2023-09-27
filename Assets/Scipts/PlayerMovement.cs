9e67ed457c9dd0e6a2f517fc69a5fd4f73b92dc5
dilara <dilara.dcelik@gmail.com>
2023-09-28 01:24:44 +0300

    attach movement animations to player

--- Assets/Scipts/PlayerMovement.cs
+++ Assets/Scipts/PlayerMovement.cs
@@ -9,11 +9,19 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float moveSpeed;
     [SerializeField] private InputActionReference movement;
     private Vector2 movementVector;
+    [SerializeField] private Animator animator;
+    private static readonly int Horizontal = Animator.StringToHash("Horizontal");
+    private static readonly int Vertical = Animator.StringToHash("Vertical");
+    private static readonly int Speed = Animator.StringToHash("Speed");
 
     // Update is called once per frame
     void Update()
     {
         movementVector = movement.action.ReadValue<Vector2>();
+        
+        animator.SetFloat(Horizontal,movementVector.x);
+        animator.SetFloat(Vertical,movementVector.y);
+        animator.SetFloat(Speed,movementVector.sqrMagnitude);
     }
 
     private void FixedUpdate()
